0176b2fd88cbb8d98c9a2fd68260a2f710993d1f
dtomsig <dtomsig@gmail.com>
2015-09-26 14:16:15 -0400

    Author: Diego Tomsig

--- Email Spider/spider.cpp
+++ Email Spider/spider.cpp
@@ -2,8 +2,8 @@
 * FILE: spider.cpp                                                             *
 *                                                                              *                                                                                     *
 * DESCRIPTION: spider.cpp is where the main method for emailSpider.exe lives.  *
-* It defines functions that are displayed when the process is run. It also     *	
-* performs the core operation of spidering webpages for emails.                *
+* It defines functions that are displayed when the process is run. It performs *	
+* the core operation of spidering webpages for emails.                         *
 *                                                                              *
 * OUTPUT FILE: emailSpider.exe                                                 *
 *******************************************************************************/
@@ -104,16 +104,52 @@ void printOptions()
 }
 
 
+/*******************************************************************************
+* FUNCTION: findEmails(std::string getResponse)                                *
+*                                                                              *                                                                                     *
+* DESCRIPTION: findEmails receives a raw GET response and returns an array of  *
+* strings containing all of the email addresses located in that GET response.  *                                             *
+*                                                                              *
+* PARAMETERS: (-getResponse: The GET response from the HTTP server that will be*
+* used to obtain email addresses.)                                             *
+*******************************************************************************/
+
+std::string* findEmails(std::string getResponse)
+{
+    //std::string emails[];
+    
+    
+  //  return emails;
+}
+
 
 /*******************************************************************************
-* FUNCTION: parse(std::string hostName)                                        *
+* FUNCTION: findUrls(std::string getResponse)                                  *
 *                                                                              *                                                                                     *
-* DESCRIPTION: parse receives .                                                *
+* DESCRIPTION: findUrls receives a raw GET response and returns an array of    *
+* strings containing all of the email addresses located in that GET response.  *                                             *
+*                                                                              *
+* PARAMETERS: (-getResponse: The GET response from the HTTP server that will be*
+* used to obtain email addresses.)                                             *
+*******************************************************************************/
+
+std::string* findUrls(std::string getResponse)
+{
+	
+}
+
+
+/*******************************************************************************
+* FUNCTION: connect(std::string hostName)                                      *
+*                                                                              *                                                                                     *
+* DESCRIPTION: connect attempts to extablish a tcp connection with the         *
+* top-level hostname. If it is unsuccessful, -1 is returned. Otherwise, the    *
+* file descriptor is returned after a connection.                              *
 *                                                                              *
 * PARAMETERS: (-hostName: The host name that will be connected to.)            *
 *******************************************************************************/
 
-std::string* parse(std::string hostName)
+int connect(std::string hostName)
 {
     /*
     ** "statusAddr" refers to the status after getaddrinfo() has been used 
@@ -124,6 +160,94 @@ std::string* parse(std::string hostName)
     **
     ** "socketfd" refers to the handle associated with creating a socket.
     **
+    ** "host_info" refers to the structure that contains address information in
+    **  order to create a socket connection to the hostName. 
+    **
+    ** "host_info_list" is a pointer to an addrinfo structure that is used in 
+    **  the getaddrinfo() function. 
+    */
+    int statusAddr, statusSocketConnect, socketfd;
+    struct addrinfo host_info;
+    struct addrinfo *host_info_list;
+    
+    /* 
+    ** These commands are necessary to fill the address info structures with 
+    ** information associated with the particular webpage.
+    */
+    memset(&host_info, 0, sizeof(host_info));
+    host_info.ai_family = AF_INET;
+    host_info.ai_socktype = SOCK_STREAM;
+    statusAddr = getaddrinfo(hostName.c_str(), "80", &host_info, 
+                             &host_info_list);
+                             
+    if(statusAddr != 0)
+    {
+        std::cout << "Get addr info error" << std::endl;
+        return -1;
+    }
+    else
+        std::cout << "No getaddrinfo error" << std::endl;
+        
+    /* 
+    ** These commands are necessary in order to create a socket given the 
+    ** address information used above. 
+    */
+    socketfd = socket((*host_info_list).ai_family, 
+                      (*host_info_list).ai_socktype, 
+                      (*host_info_list).ai_protocol);	
+                         
+    if(socketfd == -1)
+    {
+        std::cout << "Socket error" << std::endl;
+        return -1;
+    }
+    else
+        std::cout << "There is no socket error" << std::endl;
+       
+    /* 
+    ** These commands connect the socket to the web server and sends the GET 
+    ** request (stored in "msg") to the webserver. It also receives the message
+    ** and stores it in "incoming_data_buffer". 
+    */
+    statusSocketConnect = connect(socketfd, (*host_info_list).ai_addr, 
+                                  (*host_info_list).ai_addrlen);
+                                  
+    if(statusSocketConnect == -1)  
+    {
+        std::cout << "Connect error" << std::endl;
+        return -1;
+    }
+    
+    return socketfd;
+}
+
+
+/*******************************************************************************
+* FUNCTION: disconnect(int socketfd)                                           *
+*                                                                              *                                                                                     *
+* DESCRIPTION: disconnect closes the socket                                    *
+*                                                                              *
+* PARAMETERS: (-socketfd: The file descriptor of the socket that will be       *
+*               closed.)                                                       *
+*******************************************************************************/
+
+void disconnect(int socketfd)
+{
+    close(socketfd);
+}
+
+
+/*******************************************************************************
+* FUNCTION: obtainGetResponse(std::string hostName)                            *
+*                                                                              *                                                                                     *
+* DESCRIPTION: obtainGetResponse attempts to.                                  *
+*                                                                              *
+* PARAMETERS: (-hostName: The host name that will be connected to.)            *
+*******************************************************************************/
+
+std::string* obtainGetResponse(std::string url, int socketfd)
+{
+    /*
     ** "len" refers to the length of the GET message to the web server.
     **
     ** "bytes_sent" refers to the number of bytes sent to the server. 
@@ -133,87 +257,27 @@ std::string* parse(std::string hostName)
     ** "incoming_data_buffer" is an array that stores the response from the 
     **  webserver.
     **
-    ** "pageSource" refers to an entire webpage that is going to be spidered.
+    ** "pageSource" refers to the string that contains the page source.
     **
-    ** "msg" refers to the GET message sent to the webserver.
-    **
-    ** "addrinfo" refers to the structure that contains address information in
-    **  order to create a socket connection. 
-    **
-    ** "host_info_list" is a pointer to an addrinfo structure that is used in 
-    **  the getaddrinfo() function. 
+    ** "msg" refers to the GET message sent to the webserver. http://www.google.com/test/1
     */	
-    int statusAddr, statusSocketConnect, socketfd, len;
+    int len;
     ssize_t bytes_sent, bytes_received;
     char incoming_data_buffer[1448];
-    std::string pageSource,
-                msg = ("GET / HTTP/1.1\r\nHost: " + hostName + "\r\n\r\n");
-    struct addrinfo host_info;
-    struct addrinfo *host_info_list;
-    
-    /* 
-    ** These commands are necessary to fill the address info structures with 
-    ** information associated with the particular webpage.
-    */
-    memset(&host_info, 0, sizeof(host_info));
-    host_info.ai_family = PF_INET;
-    host_info.ai_socktype = SOCK_STREAM;
-    statusAddr = getaddrinfo(hostName.c_str(), "80", &host_info, 
-                             &host_info_list);
-                             
-    if(statusAddr != 0)
-    {
-        std::cout << "Get addr info error" << std::endl;
-        return NULL;
-    }
-    else
-        std::cout << "No getaddrinfo error" << std::endl;
-
-    /* 
-    ** These commands are necessary in order to create a socket given the 
-    ** address information used above. 
-    */
-    socketfd = socket((*host_info_list).ai_family, 
-                      (*host_info_list).ai_socktype, 
-                      (*host_info_list).ai_protocol);	
-    
-   
-    if(socketfd == -1)
-    {
-        std::cout << "Socket error" << std::endl;
-        return NULL;
-    }
-    else
-        std::cout << "There is no socket error" << std::endl;
-       
-     
+    std::cout << url.find("//");
+    std::string pageSource, msg = ("GET "  "HTTP/1.0\r\nHost:" 
+                                    "\r\n\r\n");                                 
+    len = strlen(msg.c_str());
+    bytes_sent = send(socketfd, msg.c_str(), len, 0);	
         
-    /* 
-    ** These commands connect the socket to the web server and sends the GET 
-    ** request (stored in "msg") to the webserver. It also receives the message
-    ** and stores it in "incoming_data_buffer". 
-    */
-    statusSocketConnect = connect(socketfd, host_info_list->ai_addr, 
-                                  host_info_list->ai_addrlen);
-    //fcntl(socketfd, F_SETFL, O_NONBLOCK);
-                                  
-    if(statusSocketConnect == -1)  
+    while((bytes_received = recv(socketfd, incoming_data_buffer, 1447, 0)))
     {
-        std::cout << "Connect error" << std::endl;
-        return NULL;
-    }
-    else
-    {
-        len = strlen(msg.c_str());
-        bytes_sent = send(socketfd, msg.c_str(), len, 0);	
-        bytes_received = recv(socketfd, incoming_data_buffer, 1447, 0);
         incoming_data_buffer[bytes_received] = '\0';
-        pageSource += incoming_data_buffer;
+        pageSource += incoming_data_buffer;      
     }
+    
     std::cout << pageSource;
-    close(socketfd);
 } 
-
 
 
 /*******************************************************************************
@@ -222,19 +286,34 @@ std::string* parse(std::string hostName)
 * DESCRIPTION: This function controls the logical level of spidering.          *
 *                                                                              *
 * PARAMETERS: (-hostName: The hostname that will be connected to.,             *
-* -outPutFile: The output file stream that will contain the store the spidered *
-* email addresses.)                                                            *
+*              -outPutFile: The output file stream that will contain the store *
+*                           the spidered  email addresses.)                    *                                           *
 *******************************************************************************/
 
-void spider(std::string hostName, std::ofstream &outputFile)
+void spider(std::string hostName, std::ofstream &outputFile, int searchDepth)
 {   
+	/*
+	** "url" represents a url that will be used for spidering that contains 
+	** the depth of the url.
+	** 
+	** "urls" represents all of the urls that remain to be spidered by the 
+	** program.
+	**
+	** "currentSocketfd" represents the current socket file descriptor that the
+	** program is connected to. 
+	*/
+    struct url 
+    {
+        std::string urlAddress;
+        int searchDepth;
+        
+        url(std::string u, int s) : urlAddress(u), searchDepth(s) {}	  
+    };
+    std::queue<url> urls;
+    int currentSocketfd;
     
-    std::queue <std::string> urls;
-    
-    /* Sets the default values for 
-	
 	/* Attempts initial connection to determine if hostname is valid. */
-    if(parse(hostName) == NULL)
+    if((currentSocketfd = connect(hostName)) && currentSocketfd != -1)
     {
 	    std::cout << termcolor::reset << termcolor::red 
 	              << "\nError: Unable to connect to specified host: \"" 
@@ -242,16 +321,14 @@ void spider(std::string hostName, std::ofstream &outputFile)
 	}
 	else
 	{
-        urls.push(hostName);
-        std::cout << "\nURL left to spider is " << urls.empty() << std::endl;
+        urls.push(url("http://" + hostName, 1));
         
         /* Main spidering loop lies here. */
-        while(urls.empty() != true)
+        while(!urls.empty())
         {
-        
+            obtainGetResponse(urls.front().urlAddress, currentSocketfd);
 	    }
     }
-    
 }
 
 int main(int argc, char **argv)
@@ -282,7 +359,7 @@ int main(int argc, char **argv)
     ** Default options are set here. These can change based upon what the user
     ** enters in as arguments. 
     */                                                                       
-    hostName = "127.0.0.1";
+    hostName = "www.google.com";
     displayHelpFlag = 1; 
     outputFileName = "emails.txt";
     searchDepth = 3;
@@ -295,7 +372,8 @@ int main(int argc, char **argv)
     { 
         {       "help",              no_argument,   &displayHelpFlag,     1},
         {            0,        required_argument,                  0,   'h'},
-        {            0,        optional_argument,                  0,   'o'},         
+        {            0,        optional_argument,                  0,   'o'}, 
+        {            0,        optional_argument,                  0,   'd'},        
         {            0,                        0,                  0,     0}
     };
 
@@ -319,6 +397,7 @@ int main(int argc, char **argv)
                 
             case('?'):
                 displayHelpFlag = 0;
+                break;
         }
     }
     
@@ -326,15 +405,17 @@ int main(int argc, char **argv)
     ** Determines what operation to perform based upon what was entered into
     ** the program. 
     */
-
     if(displayHelpFlag)
         printOptions();
+   /* else if()
+    {
+		
+	}*/
     else
 	{
 		outputFile.open(outputFileName);
-		spider(hostName, outputFile);
+		spider(hostName, outputFile, searchDepth);
 	}
 	
 	outputFile.close();
-
 }
