863bdcae88a38d120550f773c93651d9bb33e4ac
Diego Tomsig <dtomsig@gmail.com>
2024-03-13 13:14:36 -0400

    04/05/24 - Fixing Exit Codes and Typo In Function Name connectionHandlerer()
    1.  I modified the exit messages and exit codes in the main method of server.cpp and overFlowExploit.cpp. I am now using exit codes defined in sysexits.h.
    
    2. The function formerly called connectionHandlerer() has been renamed to connectionHandler(). The function name was updated any time it appeared in the code.

--- Buffer Overflow Server Example/server.cpp
+++ Buffer Overflow Server Example/server.cpp
@@ -1,9 +1,10 @@
 #include <arpa/inet.h>
 #include <iostream>
 #include <pthread.h>
-#include <string.h>
 #include <sstream>
+#include <string.h>
 #include <sys/socket.h>
+#include <sysexits.h>
 #include <unistd.h>
 
 void readData(void *socket_desc)
@@ -16,35 +17,34 @@ void readData(void *socket_desc)
     recv(*(static_cast<int*>(socket_desc)), dataBuffer, 700, 0);
     std::cout << dataBuffer << std::endl;
 }
-    
-void *connectionHandlerer(void *socket_desc)
+
+void *connectionHandler(void *socket_desc)
 {
     readData(socket_desc);
 }
 
 void createServer(int port)
 {
-    int c, socket_desc, client_sock, read_size;
+    int sock, socket_desc, client_sock, read_size;
     struct sockaddr_in server, client;
-    
+
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port  = htons(port);
-    
+    server.sin_port = htons(port);
+
     bind(socket_desc, (struct sockaddr *)&server, sizeof(server));
     listen(socket_desc, 3);
-    
+
     c = sizeof(struct sockaddr_in);
-    
-    while(client_sock = accept(socket_desc, (struct sockaddr *)&client, 
+
+    while(client_sock = accept(socket_desc, (struct sockaddr *)&client,
                               (socklen_t*)&c))
     {
         pthread_t newThread;
-        pthread_create(&newThread, NULL, &connectionHandlerer, &client_sock);
+        pthread_create(&newThread, NULL, &connectionHandler, &client_sock);
     }
-    return;
 }
 
 
@@ -52,21 +52,19 @@ int main(int argc, char **argv)
 {
     if(argc != 2)
     {
-        std::cout << "The server encountered an error while starting. "  
-                  << "Enter only one port." 
-                  << std::endl;
-        return 0;              
+        std::cout << "Invalid number of arguments." << std::endl;
+        exit(EX_USAGE);
     }
-    
+
     std::istringstream ss(argv[1]);
     int port;
-    
+
     if(!(ss >> port) || !(ss.eof()))
     {
-        std::cout << "Invalid port entered while starting the server. " 
-                  << std::endl;
-        return 0;
+        std::cout << "Port number could not be parsed from argument list." << std::endl;
+        exit(EX_USAGE);
     }
-    
+
     createServer(port);
+    return EXIT_SUCCESS;
 }
